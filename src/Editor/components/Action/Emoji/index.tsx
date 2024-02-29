9a22de363097ecb3a3575ade07c8daf8267d4014
hocgin <hocgin@gmail.com>
2024-02-29 17:35:47 +0800

    文档完善

--- src/Editor/components/Action/Emoji/index.tsx
+++ src/Editor/components/Action/Emoji/index.tsx
@@ -4,7 +4,7 @@ import TbButton from '@/Editor/components/Common/TbButton';
 import { SmileOutlined } from '@ant-design/icons';
 import { Popover } from 'antd';
 import data from '@emoji-mart/data';
-import Picker from '@emoji-mart/react';
+import EmojiPicker from 'emoji-picker-react';
 
 type EmojiProps = {
   editor?: Editor | null;
@@ -14,8 +14,8 @@ export const Emoji: React.FC<EmojiProps> = ({ editor, placement = 'top' }) => (
   <TbButton>
     <Popover placement={placement}
              trigger="click"
-             content={<Picker data={data} onEmojiSelect={(emoji) =>
-               editor?.chain().insertContent(emoji.native).run()
+             content={<EmojiPicker onEmojiClick={(emoji) =>
+               editor?.chain().insertContent(emoji.emoji).run()
              } />}>
       <SmileOutlined />
     </Popover>
