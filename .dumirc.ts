528cbc5e07cdc6773b89db8be4fc4e6ec13015ab
hocgin <hocgin@gmail.com>
2024-03-07 23:47:56 +0800

    文档完善

--- .dumirc.ts
+++ .dumirc.ts
@@ -1,8 +1,21 @@
 import { defineConfig } from 'dumi';
 
+export const useLogger = () => {
+  let result = [];
+  let offLogger = process.env.USE_LOG !== 'true';
+  console.debug(`[${offLogger ? '禁用' : '启用'}]日志打印`);
+  if (offLogger) {
+    result.push([
+      'transform-remove-console',
+      { exclude: ['error', 'warn', 'info'] },
+    ]);
+  }
+  return result;
+};
 export default defineConfig({
   outputPath: 'docs-dist',
   themeConfig: {
     name: 'editor',
   },
+  extraBabelPlugins: [...useLogger()],
 });
